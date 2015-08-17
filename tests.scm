4127760d23cd7754fe159f4eab96b81b6385bf42
William E. Byrd <webyrd@gmail.com>
2015-08-17 15:45:45 -0600

    added comments

--- tests.scm
+++ tests.scm
@@ -306,6 +306,8 @@
  bar2 (list (make-production 'pair? (list (list env bar1) (list env env)))))
 (define as (list sym num nil term1 term2 binding env foo bar1 bar2))
 (compute-non-empty* as)
+;;; Important: any productions built up using 'automaton-productions-set!' need to be passed to 'compute-non-empty*'.
+
 
 (test (unfold env '#(a)) `(((#(a) . ,env))))
 
